f784cffa5b21a0ea91a9431d279dcdf878a24b42
Varsha Shubhashri M <148562671+vvvvvvss@users.noreply.github.com>
2024-03-30 10:09:15 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,8 @@
 from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+import numpy as np
 y_true=[0,2,3,5,9]
 y_pred=[0.1, 1.7, 3.9, 5, 7]
-print('Mean Absolut error=', mean_absolute_error(y_true,y_pred))
+print('Mean Absolute error=', mean_absolute_error(y_true,y_pred))
+print('Mean Squared error=', mean_squared_error(y_true,y_pred))
+print('Root Mean Squared error=', np.sqrt( mean_squared_error(y_true,y_pred)))
